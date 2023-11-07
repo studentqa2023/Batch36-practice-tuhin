329dbc5aea73f49e2b8e20344a3c05c559462073
studentqa2023 <rezwanislam857@gmail.com>
2023-11-07 18:21:54 -0500

    Added NewClass.java in com.generic

---
+++ src/main/java/com/generic/NewClass.java
@@ -0,0 +1,11 @@
+package com.generic;
+
+public class NewClass {
+	public void getMethod() {
+		System.out.println("new method");
+	}
+	public static void main(String[] args) {
+		NewClass obj=new NewClass();
+		obj.getClass();
+	}
+}
