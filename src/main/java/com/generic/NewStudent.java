b08170d721ce64da6f2bf6c441dd72832fa3068b
studentqa2023 <rezwanislam857@gmail.com>
2023-11-07 19:58:46 -0500

    NewStudent class.java in com.generic

---
+++ src/main/java/com/generic/NewStudent.java
@@ -0,0 +1,12 @@
+package com.generic;
+
+public class NewStudent {
+	
+	public void getMethod() {
+		System.out.println("This is method name");
+	}
+public static void main(String[] args) {
+	NewStudent obj = new NewStudent();
+	obj.getMethod();
+}
+}
