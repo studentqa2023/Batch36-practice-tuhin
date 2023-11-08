c598bc7d33e852450920a80521d4d3e7b3e95c25
studentqa2023 <rezwanislam857@gmail.com>
2023-11-07 20:08:53 -0500

    AnotherNewStudent class.java in com.generic

---
+++ src/main/java/com/generic/AnotherNewStudent.java
@@ -0,0 +1,12 @@
+package com.generic;
+
+public class AnotherNewStudent {
+	
+	public void getStudent() {
+		System.out.println("This is new student class");
+	}
+public static void main(String[] args) {
+	AnotherNewStudent obj =new AnotherNewStudent();
+	obj.getStudent();
+}
+}
